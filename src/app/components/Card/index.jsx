9eddf337c78a3c50bfa0b7bded1857979b84ccfc
Leonardo Magalhaes <leo.magalha05@gmail.com>
2026-05-14 09:12:30 -0300

    pt.3

--- src/app/components/Card/index.jsx
+++ src/app/components/Card/index.jsx
@@ -1,17 +1,13 @@
 import Link from "next/link";
 import style from "./style.module.css";
 
-export default function Card({ id, nome, profissao, phone, email }) {
+export default function Card({ id, nome, profissao, phone, email, image }) {
   return (
     <Link href={`/profissionais/${id}`} className={style.link}>
       <div className={style.card}>
         <h2>{profissao}</h2>
         <div>
-          <img
-            src="/images/prof.jpg"
-            alt="foto profissional"
-            className={style.image}
-          />
+          <img src={image} alt="foto profissional" className={style.image} />
 
           <h3>{nome}</h3>
           <p>
--- src/app/profissionais/[id]/page.jsx
+++ src/app/profissionais/[id]/page.jsx
@@ -1,8 +1,15 @@
 "use client";
 import React from "react";
+import style from "./page.module.css";
+import profissionais from "../../data/profissionais.json";
 
 export default function ProfissionalProfile({ params }) {
   const { id } = React.use(params);
+  const profissional = profissionais.find((p) => p.id === Number(id));
 
-  return <h1>{id}</h1>;
+  return (
+    <div className={style.nome}>
+      <h1>{profissional ? profissional.nome : "Profissional não encontrado"}</h1>
+    </div>
+  );
 }
